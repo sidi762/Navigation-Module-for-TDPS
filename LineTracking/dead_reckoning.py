052af55aa6585c8dd0c064cf2ee1a7cb7f578794
Sidi Liang <1467329765@qq.com>
2023-04-25 01:30:34 +0800

    Update dead_reckoning (timing may have issues)

--- LineTracking/dead_reckoning.py
+++ LineTracking/dead_reckoning.py
@@ -12,10 +12,14 @@ import time
 class DeadReckoning:
 
     def __init__(self):
+        self._time_init = time.time_ns() / 1000000 # miliseconds
+        print("Dead Reckoning initialized at ", self._time_init)
         self.time_last_dead_reckoning = 0
         self.v_x_last, self.v_y_last, self.v_z_last = 0, 0, 0
         self.a_x_last, self.a_y_last, self.a_z_last = 0, 0, 0
         self.p_x_last, self.p_y_last, self.p_z_last = 0, 0, 0
+        self.velocity_x, self.velocity_y, self.velocity_z = 0, 0, 0
+        self.position_x, self.position_y, self.position_z = 0, 0, 0
 
     def dead_reckoning(self, imu):
         # In local coordinates, i.e. x component of acceleration
@@ -24,13 +28,20 @@ class DeadReckoning:
         accel_window = 0.05
         sample_count = 10
         no_acc_count = 0
-        no_movement_threshold = 5
-        time_now_dead_reckoning = time.time_ns() / 1000000000 #s
-        interval = time_now_dead_reckoning - self.time_last_dead_reckoning
+        no_movement_threshold = 4
+        time_now_dead_reckoning = time.time_ns() / 1000000 #ms
+        time_after_init = time_now_dead_reckoning - self._time_init
+        if  time_after_init < 3000:
+            #Wait for calibration
+            #print("Dead Reckoning waiting for calibration")
+            #print(3000 - time_after_init, " ms remaining")
+            return 0, 0, 0
+        interval = (time_now_dead_reckoning - self.time_last_dead_reckoning) / 1000 #seconds
 
         for i in range(sample_count):
             #Take the average to reduce error
             acc_x, acc_y, acc_z = imu.linear_acceleration() #m/s^2
+            #acc_x, acc_y, acc_z = imu.accelerometer
             a_x += acc_x
             a_y += acc_y
             a_z += acc_z
@@ -57,6 +68,7 @@ class DeadReckoning:
             self.v_z_last = 0
             no_acc_count = 0
 
+        #print(a_x)
         v_dx = a_x  * interval
         v_dy = a_y  * interval
         v_dz = a_z  * interval
@@ -68,7 +80,7 @@ class DeadReckoning:
         p_x = v_dx * interval + self.p_x_last
         p_y = v_dy * interval + self.p_y_last
         p_z = v_dz * interval + self.p_z_last
-        self.position_x, self.position_y, self.position_z = v_x, v_y, v_z
+        self.position_x, self.position_y, self.position_z = p_x, p_y, p_z
 
         self.a_x_last, self.a_y_last, self.a_z_last = a_x, a_y, a_z
         self.v_x_last, self.v_y_last, self.v_z_last = v_x, v_y, v_z
